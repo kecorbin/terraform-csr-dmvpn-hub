51fdb2b263a69679b4680c2516d2446a6d659a7f
Kevin Corbin <kcorbin@hashicorp.com>
2020-03-09 16:03:39 -0500

    add region variable

--- variables.tf
+++ variables.tf
@@ -4,6 +4,9 @@ variable "vpc_name" {
 variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
+variable "region" {
+  default = "us-east-1"
+}
 variable "public_subnet_cidr" {
   default = "10.0.1.0/24"
 }
