67f664d66ff0f8b6e6f65f91e789359a179d7c52
Kevin Corbin <kcorbin@hashicorp.com>
2020-04-05 00:46:58 -0500

    update local security group

--- security.tf
+++ security.tf
@@ -45,16 +45,19 @@ resource "aws_security_group" "allow_local" {
   name        = "allow-local"
   description = "Traffic Allowed from local VPC"
   vpc_id      = module.vpc.vpc_id
-}
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-resource "aws_security_group_rule" "allow_local" {
-  security_group_id        = aws_security_group.allow_local.id
-  source_security_group_id = aws_security_group.allow_local.id
-  type                     = "ingress"
-  from_port                = 0
-  to_port                  = 0
-  protocol                 = "-1"
-
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/8", "192.168.0.0/16"]
+  }
 }
 
 resource "aws_security_group" "private_subnet" {
